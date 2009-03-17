16816fe1d4de1fe5e9338613bae025b2fe04c8d0
Lorenzo Berni <duplo@develer.com>
2009-03-17 11:22:01 +0000

    Add a module containing the app constants

--- showtime.py
+++ showtime.py
@@ -21,32 +21,7 @@ import parse_xml_info
 from django.conf import settings
 from django.template import Template, Context
 
-MONTHS = (
-    (1, "Gennaio"),
-    (2, "Febbraio"),
-    (3, "Marzo"),
-    (4, "Aprile"),
-    (5, "Maggio"), 
-    (6, "Giugno"),
-    (7, "Luglio"),
-    (8, "Agosto"),
-    (9, "Settembre"),
-    (10, "Ottobre"),
-    (11, "Novembre"),
-    (12, "Dicembre"),
-)
-
-YEARS = (
-    2005,
-    2006,
-    2007,
-    2008,
-    2009,
-)
-
-USER = "duplo"
-PASSWORD = "duplo"
-ACHIEVOURI = "http://localhost/achievo/"
+from const import *
 
 def main():
     settings.configure(TEMPLATE_DEBUG = True)
